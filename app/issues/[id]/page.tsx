0ab40aa0bc79afaf5d3f3623d227081c9d118685
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2023-11-26 21:19:58 +0600

    preview markdown

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
+import Markdown from "react-markdown";
 
 interface Props {
   params: { id: string };
@@ -22,10 +23,10 @@ export default async function IssueDetailsPage({ params }: Props) {
       <Heading>{issue.title}</Heading>
       <Flex gap={"4"} my={"2"}>
         <IssueStatusBadge status={issue.status} />
-        <Text>{issue.description}</Text>
+        <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>
-        <p>{issue.createdAt.toDateString()}</p>
+      <Card className="prose" mt={"4"}>
+        <Markdown>{issue.description}</Markdown>
       </Card>
     </div>
   );

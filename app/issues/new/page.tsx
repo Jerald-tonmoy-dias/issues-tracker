880ede7b22a77523adf0336c6e99bacffbe586e8
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2023-11-28 20:18:35 +0600

    disable ssr and loading md editior

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button, Callout, Text, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
+import dynamic from "next/dynamic";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
@@ -12,6 +12,9 @@ import { z } from "zod";
 import ErrorMessages from "@/app/components/ErrorMessages";
 import Spinner from "@/app/components/Spinner";
 
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 type IssueForm = z.infer<typeof createIssuesSchema>;
 
 export default function NewIssuePage() {
